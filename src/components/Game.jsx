29ebb65068943babc1e6bd13558350ad4c59bab7
maxarvid <max.arvid.anderson@gmail.com>
2021-09-27 18:42:07 +0200

    WIP Adds radio checkboxes for choosing weapon with useState

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Checkbox, Container, Button, Form } from "semantic-ui-react";
 
 const Game = () => {
-  return (
-    <div id='game'>
-      <p>Choose your weapon</p>
-    </div>
-  )
-}
+  const [weapon, setWeapon] = useState("");
 
-export default Game
+  return (
+    <Container id="game">
+      <p>The value of Weapon is {weapon}</p>
+      <Button id="new-game">New Game</Button>
+      <p>Choose your weapon</p>
+      <Form>
+        <Form.Field onClick={() => setWeapon("Rock")}>
+          <Checkbox radio name="gameRadioGroup" label="Rock" />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox radio name="gameRadioGroup" label="Paper" />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox radio name="gameRadioGroup" label="Scissors" />
+        </Form.Field>
+      </Form>
+    </Container>
+  );
+};
+
+export default Game;
