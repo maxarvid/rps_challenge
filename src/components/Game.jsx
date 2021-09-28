d9fe41e1bf981eafffbae7f0c210ccd575ec4b38
maxarvid <max.arvid.anderson@gmail.com>
2021-09-28 11:55:20 +0200

    Add useEffect and function to choose computer's weapon

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,38 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Checkbox, Container, Button, Form } from "semantic-ui-react";
 
 const Game = () => {
   const [weapon, setWeapon] = useState("");
+  const [computerWeapon, setComputerWeapon] = useState("");
+
+  const newComputerChoice = () => {
+    const choices = ["rock", "paper", "scissors"];
+    setComputerWeapon(choices[Math.floor(Math.random() * 3)]);
+  };
+
+  useEffect(() => {
+    newComputerChoice();
+  }, [])
 
   return (
     <Container id="game">
       <p>The value of Weapon is {weapon}</p>
+      <p>The value of computerWeapon is {computerWeapon}</p>
       <Button id="new-game">New Game</Button>
       <p>Choose your weapon</p>
       <Form>
-        <Form.Field id='weapon-rock' onClick={() => setWeapon("Rock")}>
+        <Form.Field id="weapon-rock" onClick={() => setWeapon("rock")}>
           <Checkbox
             radio
             name="gameRadioGroup"
             label="Rock"
-            checked={weapon == "Rock"}
+            checked={weapon == "rock"}
           />
         </Form.Field>
-        <Form.Field id='weapon-paper' onClick={() => setWeapon("Paper")}>
+        <Form.Field id="weapon-paper" onClick={() => setWeapon("paper")}>
           <Checkbox
             radio
             name="gameRadioGroup"
             label="Paper"
-            checked={weapon == "Paper"}
+            checked={weapon == "paper"}
           />
         </Form.Field>
-        <Form.Field id='weapon-scissors' onClick={() => setWeapon("Scissors")}>
+        <Form.Field id="weapon-scissors" onClick={() => setWeapon("scissors")}>
           <Checkbox
             radio
             name="gameRadioGroup"
             label="Scissors"
-            checked={weapon == "Scissors"}
-           />
+            checked={weapon == "scissors"}
+          />
         </Form.Field>
       </Form>
       <Button id="play-game">Play Game</Button>
