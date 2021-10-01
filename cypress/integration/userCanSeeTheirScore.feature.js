2ad7eee14ea15746650d72ca6c1beab95826750a
maxarvid <max.arvid.anderson@gmail.com>
2021-10-01 07:19:11 +0200

    Adds test to check that score changes after play game

--- cypress/integration/userCanSeeTheirScore.feature.js
+++ cypress/integration/userCanSeeTheirScore.feature.js
@@ -1,11 +1,18 @@
-describe('User and computer score', () => {
+describe("User and computer score", () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit("/");
+    cy.get("#new-game").click();
   });
-  describe('User score', () => {
-    it('is expected to be 0 on render', () => {
-      cy.get('#score').should('contain', "Your score: 0 – Computer score: 0")
-    });
-  })
-  
-})
+  it("is expected to be 0 on render", () => {
+    cy.get("#score").should("contain", "Your score: 0 – Computer score: 0");
+  });
+
+  it("is expected to increment after a game is played", () => {
+    cy.get("#weapon-rock").click();
+    cy.get("#play-game").click();
+    cy.get("#score").should(
+      "not.contain",
+      "Your score: 0 – Computer score: 0"
+    );
+  });
+});
